64455d2fade60c63e8c5f736b37d9a40eaf9062c
TalalBenzk <benzekkitalal@gmail.fr>
2023-09-22 00:38:33 +0200

    Feat: ajout de la class pokemon et des methodes newpokemon, starter,losthp, change_pokemon et fight(!!pas fini).

--- pokemon.h
+++ pokemon.h
@@ -7,4 +7,17 @@
 
 double pocmonType (char* typeOne, char* typetwo);
 
+typedef struct {
+    char *name;
+    double hp;
+    int attack;
+    int defense;
+    int speed;
+    char *type;
+}Pokemon;
+Pokemon* newPokemon(char *name,int hp,int attack,int defense,int speed,char *type);
+Pokemon* starter();
+double losthp(Pokemon *pokemon_attacker,Pokemon *pokemon_attacked);
+Pokemon* change_pokemon(Pokemon **equipe);
+void fight(Pokemon *pokemon_ally,Pokemon *pokemon_wild,Pokemon **equipe);
 #endif //PO_C_MON_POKEMON_H
