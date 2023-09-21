8844fff4cf8951aed059b094d5df67f005a1828b
kmaign02 <kyllian.maignan1@iprospect.com>
2023-09-22 01:03:34 +0200

    Fix du CMakeLists et ajout de commentaires

--- main.c
+++ main.c
@@ -4,11 +4,14 @@
 
 int main() {
     printf("Po-C-mon!\n");
+
+    //Génération du joueur et de la map
     Player *player = newPlayer(0, 0);
     char ** map = generateMap(player);
     setSpawnPoint(map, player);
     updateMovement(map, player, ' ');
 
+    //Boucle de jeu
     while(1){
         char command;
         fflush(stdin);
