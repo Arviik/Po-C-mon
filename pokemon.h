3a98ce08c8d3dbd07b0687bbc854f696f05dd979
TalalBenzk <benzekkitalal@gmail.fr>
2023-09-22 04:41:49 +0200

    Feat: Ajout pokeball dans le main

--- pokemon.h
+++ pokemon.h
@@ -22,6 +22,9 @@ double losthp(Pokemon *pokemon_attacker,Pokemon *pokemon_attacked);
 Pokemon* change_pokemon(Pokemon **equipe);
 int  healhp (Pokemon *pokemon_ally,Pokemon *pokemon_wild,Pokemon **equipe);
 void team_hp_reset (Pokemon **equipe);
+int check_team(Pokemon **equipe);
 int fight(Pokemon *pokemon_ally,Pokemon *pokemon_wild,Pokemon **equipe);
 int run_away(Pokemon *pokemon_ally,Pokemon *pokemon_wild,Pokemon **equipe);
+int pokeball(Pokemon *pokemon_ally,Pokemon *pokemon_wild,Pokemon **equipe);
+void team_hp_reset (Pokemon **equipe);
 #endif //PO_C_MON_POKEMON_H
