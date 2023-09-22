ce4f783852822b7a6ac1cca6a75d92068f56b746
francois <totofrancois12@gmail.com>
2023-09-22 02:05:18 +0200

    healhp

--- pokemon.h
+++ pokemon.h
@@ -10,15 +10,16 @@ double pocmonType (char* typeOne, char* typetwo);
 typedef struct {
     char *name;
     double hp;
-    int hp_max;
+    double hp_max;
     int attack;
     int defense;
     int speed;
     char *type;
 }Pokemon;
-Pokemon* newPokemon(char *name,double hp,int hp_max,int attack,int defense,int speed,char *type);
+Pokemon* newPokemon(char *name,double hp,double hp_max,int attack,int defense,int speed,char *type);
 Pokemon* starter();
 double losthp(Pokemon *pokemon_attacker,Pokemon *pokemon_attacked);
 Pokemon* change_pokemon(Pokemon **equipe);
 void fight(Pokemon *pokemon_ally,Pokemon *pokemon_wild,Pokemon **equipe);
+double  healhp (double hp_max, double current_hp);
 #endif //PO_C_MON_POKEMON_H
